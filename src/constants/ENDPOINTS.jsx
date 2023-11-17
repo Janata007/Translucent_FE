ef37b94f85972fd34f1181a28e93f6a686bf9157
Jana Markovikj <jana.markovikj@endava.com>
2023-11-17 09:56:09 +0100

    user controller endpoints

--- src/constants/ENDPOINTS.jsx
+++ src/constants/ENDPOINTS.jsx
@@ -7,14 +7,15 @@ switch (process.env.NODE_ENV) {
   default:
     break;
 }
-
-const USER_ENDPOINTS = {
-  AUTHENTICATE: "authenticate",
+const USER_BASE_PATH = "users";
+export const USER_ENDPOINTS = {
+  AUTHENTICATE: "${BASE_URL}/${USER_BASE_BATH}/authenticate",
+  GET_USER: "${BASE_URL}/${USER_BASE_BATH}/simpleUser",
+  GET_USER_BY_USERNAME: "${BASE_URL}/${USER_BASE_BATH}/user",
+  GET_USER_WITH_SECTOR: "${BASE_URL}/${USER_BASE_BATH}",
+  GET_ALL_USERS: "${BASE_URL}/${USER_BASE_BATH}/all",
+  GET_ALL_USERS_IN_SECTOR: "${BASE_URL}/${USER_BASE_BATH}/all/sector",
+  GET_ALL_USERS_IN_COMPANY: "${BASE_URL}/${USER_BASE_BATH}/all/company",
+  SET_WORK_VISIBLE_FOR_USER: "${BASE_URL}/${USER_BASE_BATH}/setWorkVisible",
+  SAVE_USER: "${BASE_URL}/${USER_BASE_BATH}/save",
 };
-
-const ENDPOINTS = {
-  //USER
-  AUTHENTICATE: "${BASE_URL}/${USER_ENDPOINTS.AUTHENTICATE}",
-};
-
-export default ENPOINTS;
